bd224416be37082c5bcbd153bdc5d11e94bcf57d
hongsgo@gmail.com <hongsgo@gmail.com>
2020-03-01 14:19:16 +0900

    feat: Add skaffold.yaml and go-web-home.deploy.yaml go-web-home.svc.yaml for integrating with K8S/Skaffold

--- main.go
+++ main.go
@@ -9,7 +9,7 @@ import (
 func main() {
 	e := echo.New()
 	e.GET("/", func(c echo.Context) error {
-		return c.String(http.StatusOK, "Hello, World! 2020-03-01 13:53(KST) skaffold test")
+		return c.String(http.StatusOK, "Hello, World! 2020-03-01 13:53(KST) skaffold test2")
 	})
 	e.Logger.Fatal(e.Start(":1323"))
 }
