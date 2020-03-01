af563fd423fbf7481b59a9568db7fcc11547ecfc
hongsgo@gmail.com <hongsgo@gmail.com>
2020-03-01 14:04:50 +0900

    feat: Add skaffold.yaml and go-web-home.deploy.yaml go-web-home.svc.yaml for integrating with K8S/Skaffold

--- README.md
+++ README.md
@@ -1,5 +1,8 @@
 # go_web_home
 * How to run in Minikube
 ```bash
-minikube service hello-minikube --url
+kubectl run go-web-home --image=hongsgo/go_web_home:latest --port=1323 --restart=Always
+
+
+minikube service go-web-home --url
 ```
--- main.go
+++ main.go
@@ -9,7 +9,7 @@ import (
 func main() {
 	e := echo.New()
 	e.GET("/", func(c echo.Context) error {
-		return c.String(http.StatusOK, "Hello, World! 2020-03-01 12:52")
+		return c.String(http.StatusOK, "Hello, World! 2020-03-01 13:53(KST) skaffold test")
 	})
 	e.Logger.Fatal(e.Start(":1323"))
 }
