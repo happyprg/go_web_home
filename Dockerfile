b21f7f750bab25b996f1a611823c53397bcb6900
hongsgo@gmail.com <hongsgo@gmail.com>
2020-03-01 15:33:32 +0900

    feat: Add Views

--- Dockerfile
+++ Dockerfile
@@ -13,7 +13,8 @@ FROM alpine:latest
 RUN apk --no-cache add ca-certificates
 WORKDIR /root/
 COPY --from=builder /app/main .
-COPY static .
+COPY static ./static
+COPY template/template/html ./template/html
 # Expose port 8080 to the outside world
 EXPOSE 1323
 # Command to run the executable
--- main.go
+++ main.go
@@ -1,14 +1,34 @@
 package main
 
 import (
+	"fmt"
+	"html/template"
+	"io"
 	"net/http"
+	"time"
 
 	"github.com/labstack/echo/v4"
 	"github.com/labstack/echo/v4/middleware"
 )
 
+type Template struct {
+	templates *template.Template
+}
+
+func (t *Template) Render(w io.Writer, name string, data interface{}, c echo.Context) error {
+	return t.templates.ExecuteTemplate(w, name, data)
+}
 func main() {
+
 	e := echo.New()
+	t := &Template{
+		templates: template.Must(template.ParseGlob("template/views/*.html")),
+	}
+	e.Renderer = t
+	e.GET("/", func(c echo.Context) error {
+		return c.Render(http.StatusOK, "serverTime", fmt.Sprintf("%s", time.Now()))
+	})
+
 	e.GET("/health", func(c echo.Context) error {
 		return c.String(http.StatusOK, "I'm alive")
 	})
@@ -18,8 +38,5 @@ func main() {
 
 	e.Static("/static", "static")
 
-	e.GET("/", func(c echo.Context) error {
-		return c.String(http.StatusOK, "Hello, World! add static")
-	})
 	e.Logger.Fatal(e.Start(":1323"))
 }
